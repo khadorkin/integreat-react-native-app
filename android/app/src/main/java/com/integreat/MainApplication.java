896d8828ac48250e1b89842aa2f1eac5b1201a58
Maximilian Ammann <max@maxammann.org>
2019-01-09 18:00:11 +0100

    NATIVE-63: Add react-native-sqlite-storage and sql-bricks

--- android/app/src/main/java/com/integreat/MainApplication.java
+++ android/app/src/main/java/com/integreat/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import org.pgsqlite.SQLitePluginPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
                     new RNCWebViewPackage(),
                     new RNFetchBlobPackage(),
                     new VectorIconsPackage(),
+                    new SQLitePluginPackage(),
                     new IntegreatPackage()
             );
         }
